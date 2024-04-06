12a13d33cc281aa13fcecdca37017b1af61e53aa
Tomgxz <thomaslee10.TL@gmail.com>
2024-04-06 15:11:20 +0100

    Consolidated toggle functions

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,89 @@
 // CONSTANTS / VARIABLES
 
 const toggles_wrapper = $("main .content-wrapper .toggle-panel"),
-  elements_wrapper = $("main .content-wrapper .element-panel"),
+      elements_wrapper = $("main .content-wrapper .element-panel"),
 
-  map = $("#map"),
-  map_svg = map.children("svg"),
+      toggle_el_stations = elements_wrapper.children(".stations"),
+      toggle_el_stationnames = elements_wrapper.children(".stationnames"),
+      toggle_el_stepfree = elements_wrapper.children(".stepfree"),
+      toggle_el_farezones = elements_wrapper.children(".farezones"),
+      toggle_el_river = elements_wrapper.children(".river"),
+      toggle_el_showall = elements_wrapper.children(".showall"),
+      toggle_el_hideall = elements_wrapper.children(".hideall"),
+      
+      map = $("#map"),
+      map_svg = map.children("svg"),
+      trains_wrapper = map_svg.children("#TRAINS-WRAPPER"),
 
-  lines = ["bakerloo", "central", "circle", "district", "hc", "jubilee", "metropolitan", "northern", "piccadilly",
-    "victoria", "wc", "dlr", "cablecar", "overground", "elizabeth", "thameslink", "tram"];
+      lines = ["bakerloo", "central", "circle", "district", "hc", "jubilee", "metropolitan", "northern", "piccadilly",
+        "victoria", "wc", "dlr", "cablecar", "overground", "elizabeth", "thameslink", "tram"];
 
+var active_trains = {},
 
     stationsEnabled = false,
     stationNamesEnabled = false,
     stepfreeEnabled = false;
+
+
+const toggle_fn_stations = () => {
+  stationsEnabled = !stationsEnabled
+
+  map.toggleClass("stations")
+  toggle_el_stations.toggleClass("enabled")
+
+  if (!stationsEnabled) {
+    map.removeClass("stationnames","stepfree")
+    toggle_el_stationnames.removeClass("enabled")
+    toggle_el_stepfree.removeClass("enabled")
+
+    stationNamesEnabled = false
+    stepfreeEnabled = false
+  }
+}
+
+const toggle_fn_stationnames = () => {
+  stationNamesEnabled = !stationNamesEnabled
+
+  map.toggleClass("stationnames")
+  toggle_el_stationnames.toggleClass("enabled")
+
+  if (stationNamesEnabled) {
+    map.addClass("stations")
+    toggle_el_stations.addClass("enabled")
+    stationsEnabled = true
+  }
+}
+
+const toggle_fn_stepfree = () => {
+  stepfreeEnabled = !stepfreeEnabled
+
+  map.toggleClass("stepfree")
+  toggle_el_stepfree.toggleClass("enabled")
+
+  if (stepfreeEnabled) {
+    map.addClass("stations")
+    toggle_el_stations.addClass("enabled")
+    stationsEnabled = true
+  }
+}
+
+// ------
+
+const toggle_fn_farezones = () => {
+  map.toggleClass("farezones")
+  toggle_el_farezones.toggleClass("enabled")
+}
+
+const toggle_fn_river = () => {
+  map.toggleClass("river")
+  toggle_el_river.toggleClass("enabled")
+}
+
+// ------
+
+const toggle_fn_showall = () => { map.addClass(lines) }
+const toggle_fn_hideall = () => {  map.removeClass(lines) }
+
 
 $(window).on("load", function () {
 
@@ -31,69 +102,21 @@ $(window).on("load", function () {
     })
   }
 
-
-  // -------------
-
-  elements_wrapper.children(".stations").click(function () {
-    stationsEnabled = !stationsEnabled
-    map.toggleClass("stations")
-    $(this).toggleClass("enabled")
-
-    if (!stationsEnabled) {
-      map.removeClass("stationnames")
-      elements_wrapper.children(".stationnames").removeClass("enabled")
-      map.removeClass("stepfree")
-      elements_wrapper.children(".stepfree").removeClass("enabled")
-      stationNamesEnabled = false
-      stepfreeEnabled = false
-    }
-  })
-
-  elements_wrapper.children(".stationnames").click(function () {
-    stationNamesEnabled = !stationNamesEnabled
-    map.toggleClass("stationnames")
-    $(this).toggleClass("enabled")
-
-    if (stationNamesEnabled) {
-      map.addClass("stations")
-      elements_wrapper.children(".stations").addClass("enabled")
-      stationsEnabled = true
-    }
-  })
+  toggle_el_stations.click(toggle_fn_stations)
+  toggle_el_stationnames.click(toggle_fn_stationnames)
+  toggle_el_stepfree.click(toggle_fn_stepfree)
   
-  elements_wrapper.children(".stepfree").click(function () {
-    stepfreeEnabled = !stepfreeEnabled
-    map.toggleClass("stepfree")
-    $(this).toggleClass("enabled")
-
-    if (stepfreeEnabled) {
-      map.addClass("stations")
-      elements_wrapper.children(".stations").addClass("enabled")
-      stationsEnabled = true
-    }
-  })
+  toggle_el_farezones.click(toggle_fn_farezones)
+  toggle_el_river.click(toggle_fn_river)
   
-  // -------------
+  toggle_el_showall.click(toggle_fn_showall)
+  toggle_el_hideall.click(toggle_fn_hideall)
 
-  elements_wrapper.children(".farezones").click(function () {
-    map.toggleClass("farezones")
-    $(this).toggleClass("enabled")
-  })
+  toggle_fn_showall()
 
-  elements_wrapper.children(".river").click(function () {
-    map.toggleClass("river")
-    $(this).toggleClass("enabled")
-  })
-  
-  // -------------
-
-  elements_wrapper.children(".showall").click(function () {
-    map.addClass(lines)
-  })
-  
-  elements_wrapper.children(".hideall").click(function () {
-    map.removeClass(lines)
-  })
+  toggle_fn_stations()
+  toggle_fn_stationnames()
+  toggle_fn_stepfree()
 
   // #endregion
 
